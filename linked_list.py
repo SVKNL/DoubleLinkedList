6cb7d05d8ffda82a882466c75c8da5fe33ee1f9b
admin <karpovv1994@icloud.com>
2025-04-30 12:47:06 +0500

    review1 fixes

--- linked_list.py
+++ linked_list.py
@@ -1,74 +1,81 @@
+from typing import Optional
+
+
+
 class DoubleLinkedList:
 
     class Node:
         def __init__(self, value):
             self.value = value
-            self.next = None
-            self.prev = None
+            self.next: Optional['Node']= None
+            self.prev: Optional['Node'] = None
 
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.size = 0
+        self.__head: Optional['Node'] = None
+        self.__tail: Optional['Node'] = None
+        self.__size = 0
 
     def append(self, value):
         node = self.Node(value)
-        if self.head is None:
-            self.head = node
-            self.tail = node
+        if self.__head is None:
+            self.__head = node
+            self.__tail = node
         else:
-            node.prev = self.tail
-            self.tail.next = node
-            self.tail = node
-        self.size += 1
+            node.prev = self.__tail
+            self.__tail.next = node
+            self.__tail = node
+        self.__size += 1
 
     def prepend(self, value):
         node = self.Node(value)
-        if self.head is None:
-            self.head = node
-            self.tail = node
+        if self.__head is None:
+            self.__head = node
+            self.__tail = node
         else:
-            node.next = self.head
-            self.head.prev = node
-            self.head = node
-        self.size += 1
+            node.next = self.__head
+            self.__head.prev = node
+            self.__head = node
+        self.__size += 1
+
+    def _insert(self, value, index):
+        node = self.Node(value)
+        target_pointer = self.__head
+        for i in range(index):
+            target_pointer = target_pointer.next
+        node.next = target_pointer
+        node.prev = target_pointer.prev
+        target_pointer.prev = node
+        target_pointer.prev.next = node
 
     def insert(self, value, index):
-        if index > self.size or index < 0:
+        if index > self.__size or index < 0:
             raise ValueError('Index out of range')
         elif index == 0:
             self.prepend(value)
-        elif index == self.size:
+        elif index == self.__size:
             self.append(value)
         else:
-            node = self.Node(value)
-            target_pointer = self.head
-            for i in range(index):
-                target_pointer = target_pointer.next
-            node.next = target_pointer
-            node.prev = target_pointer.prev
-            target_pointer.prev = node
-            target_pointer.prev.next = node
-        self.size += 1
+            self._insert(value, index)
+        self.__size += 1
 
     def delete(self, value):
-        target_pinter = self.head
+        target_pinter = self.__head
         while target_pinter:
             if target_pinter.value == value:
                 if target_pinter.prev is None:
-                    self.head = target_pinter.next
+                    self.__head = target_pinter.next
                 elif target_pinter.next is None:
-                    self.tail = target_pinter.prev
+                    self.__tail = target_pinter.prev
                 else:
                     target_pinter.prev.next = target_pinter.next
                     target_pinter.next.prev = target_pinter.prev
-                self.size -= 1
+                self.__size -= 1
                 return
             target_pinter = target_pinter.next
 
     def find(self, value):
         index = 0
-        target_pinter = self.head
+        target_pinter = self.__head
         while target_pinter:
             if target_pinter.value == value:
                 return index
@@ -77,10 +84,10 @@ class DoubleLinkedList:
         return -1
 
     def __len__(self):
-        return self.size
+        return self.__size
 
     def __iter__(self):
-        target_pinter = self.head
+        target_pinter = self.__head
         while target_pinter:
             yield target_pinter.value
             target_pinter = target_pinter.next
